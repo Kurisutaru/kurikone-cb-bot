2d1728ea9ec8618265636f474b711ad3fe59ea28
Varenchinusu Michael <michael.hansetyanto@kurisutaru.net>
2025-04-12 23:15:38 +0700

    locales.py : i18nice
    Implementation of commit title i18n: Moving from pyi18n to i18nice

--- archive/locales.py
+++ archive/locales.py
@@ -1,15 +1,21 @@
-import discord
-import i18n
-from attrs import define
 from discord import app_commands
 from discord.app_commands import locale_str, TranslationContextLocation
+from pyi18n import PyI18n
+import discord
+from pyi18n.loaders import PyI18nYamlLoader
+
+###
+# Kuri : Archived locales using https://github.com/sectasy0/pyi18n/
+# The reason changed that the package have glitch if you put the "Key" = Yes / No
+# It will be not loaded on the translation.
+###
 
 # Guild Locale on startup, should be guild_local[guild_id] = lang
 guild_locale = {}
 available_locales = []
 default_locale = discord.enums.Locale.american_english.value.lower()
 
-@define
+
 class Locale:
     _instance = None
     _locale = None
@@ -17,25 +23,24 @@ class Locale:
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super().__new__(cls)
-            i18n.load_path.append("locales")
-            i18n.set('filename_format', '{locale}.{format}')
-            i18n.set('file_format', 'yaml')
-            i18n.load_everything()
-
-            cls._locale = i18n
+            for locale in discord.enums.Locale:
+                available_locales.append(locale.value.lower())
+            loader : PyI18nYamlLoader = PyI18nYamlLoader()
+            i18n: PyI18n = PyI18n(available_locales=tuple(available_locales), loader=loader)
+            cls._locale = i18n.gettext
 
         return cls._instance
 
-    def get_text(self, guild_id: int, text: str, **kwargs) -> str:
+    def get_text(self, guild_id: int, string: str, **kwargs) -> str:
         lang = guild_locale.get(guild_id, default_locale)
-        return self._locale.t(key=text, locale=lang, **kwargs)
+        return self._instance._locale(lang, string, **kwargs)
 
-    def t(self, guild_id: int, text: str, **kwargs) -> str:
+    def t(self, guild_id: int, string: str, **kwargs) -> str:
         lang = guild_locale.get(guild_id, default_locale)
-        return self._locale.t(key=text, locale=lang, **kwargs)
+        return self._instance._locale(lang, string, **kwargs)
 
-    def text(self, text: str, lang: str, **kwargs) -> str:
-        return self._locale.t(key=text, locale=lang, **kwargs)
+    def text(self, locale: str, string: str) -> str:
+        return self._instance._locale(locale, string)
 
 
 class DiscordTranslator(app_commands.Translator):
@@ -59,13 +64,14 @@ class DiscordTranslator(app_commands.Translator):
 
     def _translate_command(self, context, string, lang):
         command = context.data.name
-        return self.locale.text(text=f"commands.{command}.name", lang= lang) or string
+        return self.locale.text(lang, f"commands.{command}.name") or string
 
     def _translate_command_description(self, context, string, lang):
         command = context.data.name
-        return self.locale.text(text=f"commands.{command}.description", lang= lang) or string
+        return self.locale.text(lang, f"commands.{command}.description") or string
 
     # def _translate_parameter(self, context, string, lang):
     #     command = context.data.command.name
     #     parameter = context.data.name
     #     return i18n.t(f"commands.{command}.params.{parameter}", locale=lang) or string
+
