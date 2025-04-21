31132a0c737fdfef37ac1577708925e7ef32aa26
Varenchinusu Michael <michael.hansetyanto@kurisutaru.net>
2025-04-21 09:20:38 +0700

    Unit Test : First step !
    Hopefully I can finish before release public version (non dev)

--- test/conftest.py
+++ test/conftest.py
@@ -1,5 +1,9 @@
 import pytest
-from unittest.mock import AsyncMock
+from unittest.mock import AsyncMock, MagicMock
+
+from discord import Message, TextChannel, Guild
+
+from logger import KuriLogger
 
 author_id = 1
 guild_id = 1
@@ -9,7 +13,7 @@ message_id = 1
 @pytest.fixture
 def mock_ctx():
     """Mock Discord context"""
-    ctx = AsyncMock()
+    ctx = AsyncMock(spec=Guild)
     ctx.author.id = author_id
     ctx.guild.id = guild_id
     return ctx
@@ -17,13 +21,13 @@ def mock_ctx():
 @pytest.fixture
 def mock_channel():
     """Standardized mock channel for message tests"""
-    channel = AsyncMock()
+    channel = AsyncMock(spec=TextChannel)
     channel.id = channel_id
     return channel
 
 @pytest.fixture
 def mock_message():
     """Standardized mock message tests"""
-    message = AsyncMock()
+    message = AsyncMock(spec=Message)
     message.id = message_id  # Consistent test ID
     return message
--- test/unit/test_utils.py
+++ test/unit/test_utils.py
@@ -2,7 +2,9 @@ import discord
 import pytest
 from unittest.mock import AsyncMock, patch, MagicMock
 
-from discord import TextChannel, Interaction
+from discord import TextChannel, Interaction, Embed
+from discord.ui import View
+
 # Patch all problematic imports before importing the module
 with patch.dict('sys.modules', {
     'config': MagicMock(),
@@ -10,9 +12,12 @@ with patch.dict('sys.modules', {
     'enums': MagicMock(),
     'models': MagicMock()
 }):
+    import utils
     from utils import discord_try_fetch_message, discord_close_response, send_message, send_message_short, \
-    send_message_medium, send_message_long
-    from globals import logger
+    send_message_medium, send_message_long, create_message_param, send_channel_message, send_channel_message_medium, \
+    send_channel_message_long, send_channel_message_short, send_followup_short, send_followup_medium, \
+    send_followup_long
+
 
 @pytest.mark.asyncio
 async def test_discord_try_fetch_message_success():
@@ -24,6 +29,7 @@ async def test_discord_try_fetch_message_success():
 
     mock_channel.fetch_message.assert_awaited_once_with(123)
     assert result == mock_message
+
 
 @pytest.mark.asyncio
 async def test_discord_try_fetch_message_not_found():
@@ -41,11 +47,13 @@ async def test_discord_close_response_success():
     mock_interaction.response.defer = AsyncMock()
     mock_interaction.delete_original_response = AsyncMock()
 
+    utils.log = AsyncMock()
+
     await discord_close_response(mock_interaction)
-    log = logger
     mock_interaction.response.defer.assert_awaited_once_with(ephemeral=True)
     mock_interaction.delete_original_response.assert_awaited_once()
-    log.error.assert_not_called()
+    utils.log.error.assert_not_called()
+
 
 
 @pytest.mark.asyncio
@@ -54,9 +62,12 @@ async def test_discord_close_response_failure():
     mock_interaction.response.defer = AsyncMock(side_effect=Exception("Test error"))
     mock_interaction.delete_original_response = AsyncMock()
 
+    utils.log = AsyncMock()
+
     await discord_close_response(mock_interaction)
-    log = logger
-    log.error.assert_called()
+    utils.log.error.assert_called_once()
+    mock_interaction.delete_original_response.assert_not_awaited()
+
 
 @pytest.mark.asyncio
 async def test_discord_send_messages():
@@ -74,3 +85,66 @@ async def test_discord_send_messages():
 
     await send_message_long(mock_interaction, "", False, None, None, None, None)
     mock_interaction.response.send_message.assert_called()
+
+
+@pytest.mark.asyncio
+async def test_discord_send_channel_messages():
+    mock_interaction = AsyncMock(spec=Interaction)
+    mock_interaction.channel.send = AsyncMock()
+
+    await send_channel_message(mock_interaction, "", False, None, None, None)
+    mock_interaction.channel.send.assert_called()
+
+    await send_channel_message_short(mock_interaction, "", False, None, None, None)
+    mock_interaction.channel.send.assert_called()
+
+    await send_channel_message_medium(mock_interaction, "", False, None, None, None)
+    mock_interaction.channel.send.assert_called()
+
+    await send_channel_message_long(mock_interaction, "", False, None, None, None)
+    mock_interaction.channel.send.assert_called()
+
+@pytest.mark.asyncio
+async def test_discord_send_followup_messages(mock_message):
+    mock_interaction = AsyncMock(spec=Interaction)
+    mock_interaction.followup.send = AsyncMock()
+    mock_interaction.followup.send.return_value = mock_message
+
+    await send_followup_short(mock_interaction, "", False)
+    mock_interaction.followup.send.assert_called()
+
+    await send_followup_medium(mock_interaction, "", False)
+    mock_interaction.followup.send.assert_called()
+
+    await send_followup_long(mock_interaction, "", False)
+    mock_interaction.followup.send.assert_called()
+
+
+
+
+def test_create_message_param():
+    mock_embed = MagicMock(Embed)
+    mock_embeds = MagicMock(list[Embed])
+    mock_embeds.__len__.return_value = 1
+    mock_view = MagicMock(View)
+
+    param = create_message_param(content="123")
+    assert param["content"] == "123"
+
+    param = create_message_param(content="123", ephemeral=True)
+    assert param["ephemeral"] is True
+
+    param = create_message_param(content="123", embed=mock_embed)
+    assert param["embeds"] is not None
+
+    param = create_message_param(content="123", embeds=mock_embeds)
+    assert param["embeds"] is not None
+
+    param = create_message_param(content="123", view=mock_view)
+    assert param["view"] is not None
+
+    param = create_message_param(content="123", delete_after=1)
+    assert param["delete_after"] == 1
+
+    param = create_message_param(content="123", silent=True)
+    assert param["silent"] is True
